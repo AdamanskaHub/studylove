d1b0e306d38a5a5f34fa96b715e45c5b66ab3ce6
Emilie <adamanska@gmail.com>
2021-07-07 13:15:06 +0200

    exporting the json and accessing

---
+++ pages/jay.js
@@ -0,0 +1,172 @@
+// export const jay = [
+// 	{
+// 		"basic":
+// 			[
+// 				[ // lv1
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 0" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog 0" },
+// 					],
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 1" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog b1" },
+// 					],
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 2" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog 2" },
+// 					]
+// 				],
+// 				[ // lv
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 1.0" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog 1.0" },
+// 					],
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 1.1" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog b1.1" },
+// 					],
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 1.2" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog 1.2" },
+// 					]
+// 				],
+// 				[// lv
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 2.0" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog 2.0" },
+// 					],
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 2.1" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog b2.1" },
+// 					],
+// 					[
+// 						{ "pic": "/jay_neutral.svg", "txt": "ske 2.2" },
+// 						{ "pic": "/jay_neutral.svg", "txt": "frog 2.2" },
+// 					]
+// 				]
+// 			],
+
+
+// "studyOn" : [
+// 	[
+// 		// lv
+// 		[
+// 			{ "pic": "/jay_frown.svg", "txt": "STUDY ON 0" },
+// 			{ "pic": "/jay_frown.svg", "txt": "STUDY ON 0" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_frown.svg", "txt": "STUDY ON 1" },
+// 			{ "pic": "/jay_frown.svg", "txt": "STUDY ON b1" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_frown.svg", "txt": "STUDY ON 2" },
+// 			{ "pic": "/jay_frown.svg", "txt": "STUDY ON b2" },
+// 		]
+// 	],
+// 	[
+		
+// // lv
+// 		[
+// 			{ "pic": "/jay_frown.svg", "txt": "ske 1.0" },
+// 			{ "pic": "/jay_frown.svg", "txt": "frog 1.0" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_frown.svg", "txt": "ske 1.1" },
+// 			{ "pic": "/jay_frown.svg", "txt": "frog b1.1" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_frown.svg", "txt": "ske 1.2" },
+// 			{ "pic": "/jay_frown.svg", "txt": "frog 1.2" },
+// 		]
+// 	]
+// ],
+// "welcome" : [
+// 	[
+// 		// lv
+// 		[
+// 			{
+// 				"pic": "/jay_neutral.svg",
+// 				"txt": "welcome welcome",
+// 			},
+// 			{ "pic": "/jay_smirk.svg", "txt": "AAAAAAH 0" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 1" },
+// 			{ "pic": "/jay_smirk.svg", "txt": "AAAAAAH b1" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 2" },
+// 			{ "pic": "/jay_neutral.svg", "txt": "AAAAAAH 2" },
+// 		]
+// 	],
+// 	[
+// 		// lv
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 1.0" },
+// 			{ "pic": "/jay_neutral.svg", "txt": "AAAAAAH 1.0" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 1.1" },
+// 			{ "pic": "/jay_neutral.svg", "txt": "AAAAAAH b1.1" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 1.2" },
+// 			{ "pic": "/jay_neutral.svg", "txt": "AAAAAAH 1.2" },
+// 		]
+// 	],
+// 	[
+// 		// lv
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 2.0" },
+// 			{ "pic": "/jay_neutral.svg", "txt": "AAAAAAH 2.0" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 2.1" },
+// 			{ "pic": "/jay_neutral.svg", "txt": "AAAAAAH b2.1" },
+// 		],
+// 		[
+// 			{ "pic": "/jay_neutral.svg", "txt": "welcome 2.2" },
+// 			{ "pic": "/jay_neutral.svg", "txt": "AAAAAAH 2.2" },
+// 		]
+// 	]
+// ],
+// "keepGoing" : [
+  
+// 	[
+// 		// lv
+// 		[
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue A" },
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue B" },
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue C" },
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue D" },
+// 		]
+// 	],
+// 	[
+// 	// lv
+// 		[
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue XXX A" },
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue XXX B" },
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue XXX C" },
+// 			{ "pic": "/jay_smirk.svg", "txt": "continue XXX D" },
+// 		]
+// 	]
+// ]
+// }
+// ]
+
+
+
+export const jay = [
+	{
+		id: [
+			{ 'other': 'KKKKKJJJJHHHH' },
+			{ 'other': 'otherrrruuuuuu' },
+		]
+	},
+	{
+		zz: [
+			{ 'other': 'otr2' },
+			{'other': 'otter'}
+		]
+	}
+]
